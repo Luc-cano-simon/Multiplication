79088c7f1931145ab3419d363f15b38829f7fd47
luc <luc.cano@hotmail.fr>
2018-06-26 14:25:08 +0200

    debut de code

---
+++ app.js
@@ -0,0 +1,29 @@
+var ColorTab ;
+
+	document.write("<table border='5px'>");
+
+	for(var i = 1; i < 11; i++) 
+	{
+
+	document.write("<tr style='height:50px;'>");
+
+	for(var j = 1; j < 11; j++) 
+	{
+
+		if(j == 1 || i == 1) 
+		{
+			
+		
+		 }else {
+			
+		}
+	
+	document.write("<td style='width:50px;'"  +ColorTab+  "'>"  +i*j+  "</td>");
+		
+	}
+
+}
+
+
+
+
